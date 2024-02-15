e5648d3ca6f180b974323245c8fd33709f5387e7
Dev Bhatia <152610470+DevBhatia22@users.noreply.github.com>
2024-02-15 11:13:29 +0000

    changed

--- Easy/2529.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maximumCount(self, nums: List[int]) -> int:
-        d,e=0,0
-        for v in nums:
-            if v>0:d+=1
-            elif v<0:e+=1
-        if d>e:return d
-        else:return e
